bd5f0757dfb745d822defd1f91b51a1bdcee39e9
Logan Moss <lrdjmoss@gmail.com>
2020-04-24 13:12:43 -0700

    Made buttons open up in new tab

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -5,7 +5,7 @@ function PreviewCard(props) {
   
   let button;
   if(props.deployed_url !== ""){
-    button = <a href={props.deployed_url} className="btn btn-primary">{props.deployed_tag}</a>
+    button = <a href={props.deployed_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">{props.deployed_tag}</a>
   }
 
   return (
@@ -17,7 +17,7 @@ function PreviewCard(props) {
       </div>
       <div>
         {button}
-        <a href={props.github_url} className="btn btn-primary">Github</a>
+        <a href={props.github_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Github</a>
       </div>
     </div>
   );
