5b042ba3b4f4079759bbffc566f41c1ce0dbd4be
Logan Moss <lrdjmoss@gmail.com>
2020-04-23 22:39:17 -0700

    Cleared README

---
+++ build/precache-manifest.1efc5b3a966078eec47e2aedfba976ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497570ec9c7c174d8ad3098e8ded8a03",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5f70bcc0fb33c0386f12",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "5d77da100f5ad180e353",
+    "url": "/portfolio/static/css/main.69009b07.chunk.css"
+  },
+  {
+    "revision": "5f70bcc0fb33c0386f12",
+    "url": "/portfolio/static/js/2.1e79e6b5.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.1e79e6b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d77da100f5ad180e353",
+    "url": "/portfolio/static/js/main.c812b3d4.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
