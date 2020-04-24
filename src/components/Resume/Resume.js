0658cda40b28cc7216d6d91ccb964a26d76239cd
Logan Moss <lrdjmoss@gmail.com>
2020-04-23 18:46:32 -0700

    Added file updates

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import "../../App.css"
+import React, { Component } from 'react';
+import "../../App.css";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function Resume() {
-  return (
-    <h1>Resume Page</h1>
-  );
+class Resume extends Component {
+
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render(){
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <div>
+        <Document 
+          file="/Users/lrmoss/Desktop/react-portfolio/portfolio/public/LoganResumeMarch2020.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
+  
 }
 
 export default Resume;
