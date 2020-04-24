fb4369a177aa4b1a274dfda2a2e0ca6ada456ae0
Logan Moss <lrdjmoss@gmail.com>
2020-04-23 17:27:09 -0700

    Added updated information

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -5,12 +5,12 @@ function PreviewCard(props) {
 
   return (
     <div className="card">
-      <img className="card-img-top" src={props.image} alt={props.alt} height="90%" width="90%"></img>
+      <img className="card-img-top" src={props.image} alt={props.alt} height="250px" width="400px"></img>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text">{props.description}</p>
-        <a href={props.url} className="btn btn-primary">{props.linktype}</a>
       </div>
+        <a href={props.url} className="btn btn-primary">{props.linktype}</a>
     </div>
   );
 }
