813ab7c09afcad07495d21ae4d9d99693072602e
Logan Moss <lrdjmoss@gmail.com>
2020-05-01 00:20:52 -0700

    Updates

--- precache-manifest.6a7253b6598b7a3838171aa01b7b8686.js
+++ precache-manifest.6a7253b6598b7a3838171aa01b7b8686.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcd439d3b9eaf14583376661fe3e7ba2",
+    "revision": "6966785e4210139a1c3d84940d090880",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
   },
   {
-    "revision": "d9dcec3eeefe75598d00",
+    "revision": "6900bc7487ba115e98de",
     "url": "/portfolio/static/css/main.fc05c226.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.01c9e13e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9dcec3eeefe75598d00",
-    "url": "/portfolio/static/js/main.3ba10006.chunk.js"
+    "revision": "6900bc7487ba115e98de",
+    "url": "/portfolio/static/js/main.b35d7055.chunk.js"
   },
   {
     "revision": "e5a10efc7de947a9cedd",
