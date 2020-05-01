57f467e8b22ef3d7202c0b9ff3e7c50e188541b8
Logan Moss <lrdjmoss@gmail.com>
2020-05-01 00:24:37 -0700

    Bugfixing router deployed bugs

--- build/precache-manifest.43cb0c494cd26c4ed3567a5d8483d791.js
+++ build/precache-manifest.43cb0c494cd26c4ed3567a5d8483d791.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6966785e4210139a1c3d84940d090880",
+    "revision": "5d8953e00b88093b619724b49275ac2e",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
   },
   {
-    "revision": "6900bc7487ba115e98de",
+    "revision": "926af5c15c906483504d",
     "url": "/portfolio/static/css/main.fc05c226.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.01c9e13e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6900bc7487ba115e98de",
-    "url": "/portfolio/static/js/main.b35d7055.chunk.js"
+    "revision": "926af5c15c906483504d",
+    "url": "/portfolio/static/js/main.3b6f0900.chunk.js"
   },
   {
     "revision": "e5a10efc7de947a9cedd",
