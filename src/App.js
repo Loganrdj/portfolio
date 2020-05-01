76eb133221c6259ef4917066a267d140f1f56b13
Logan Moss <lrdjmoss@gmail.com>
2020-05-01 00:24:37 -0700

    Bugfixing router deployed bugs

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ class App extends Component {
         <div className="App">
           <Nav />
           <Switch>
-            <Route exact path="/portfolio" component={Home}/>
+            <Route path="/portfolio" component={Home}/>
             <Route exact path="/" component={Home}/>
-            <Route exact path="/resume" component={Resume}/>
-            <Route exact path="/projects" component={Projects}/>
-            <Route exact path="/contact" component={Contact}/>
+            <Route path="/resume" component={Resume}/>
+            <Route path="/projects" component={Projects}/>
+            <Route path="/contact" component={Contact}/>
           </Switch>
         </div>
       </Router>
