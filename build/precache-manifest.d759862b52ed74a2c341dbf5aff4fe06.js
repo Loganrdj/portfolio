22f7163d8c829377106f4140271a9062404ee5d7
Logan Moss <lrdjmoss@gmail.com>
2020-05-09 10:31:34 -0700

    Deploying

---
+++ build/precache-manifest.d759862b52ed74a2c341dbf5aff4fe06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2646dc0b24d0a99046dd6b540dcc7a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c69d14a1d1146051885d",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "505ae91f934be7676847",
+    "url": "/portfolio/static/css/main.23ea7276.chunk.css"
+  },
+  {
+    "revision": "c69d14a1d1146051885d",
+    "url": "/portfolio/static/js/2.2dcde6c5.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.2dcde6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505ae91f934be7676847",
+    "url": "/portfolio/static/js/main.41e515db.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
