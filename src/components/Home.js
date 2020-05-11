ca8d3fe949bd2946f7f30edaf1fa8e175eaa2a12
Logan Moss <lrdjmoss@gmail.com>
2020-05-10 19:30:11 -0700

    Added skills

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ class Home extends Component{
             <h1 class="display-4 textAnimate">Hello.</h1>
           {/* </FadeIn>
           <FadeIn delay={2000} transitionDuration={4000}> */}
-            <h2 class="textAnimate">My name is Logan Moss and I am a Fullstack Developer</h2>
+            <h2 className="textAnimate">My name is Logan Moss and I am a Fullstack Developer</h2>
             <hr className="my-4"></hr>
             <div className="row">
               <div className="col-md-3 col-lg-3 col-sm-3 col-xl-3">
@@ -23,7 +23,7 @@ class Home extends Component{
               </div>
               <div className="col-md-3 col-lg-3 col-sm-3 col-xl-3">
               <Link to="/projects" className="nav-link">
-                <button className="buttonClass buttonAnimation">Portfolio</button>
+                <button className="buttonClass buttonAnimation">Projects</button>
               </Link>
               </div>
               <div className="col-md-3 col-lg-3 col-sm-3 col-xl-3">
@@ -35,7 +35,48 @@ class Home extends Component{
                 <button className="buttonClass buttonAnimation">Github</button>
               </div>
             </div>
-            
+            <hr className="my-4"></hr>
+            <h2 className="textAnimate">My skills include:</h2>
+            <div className="row">
+              <div className="col-md-4">
+                  <div className="containe textAnimate">
+                  <FadeIn>
+                    <h4>Python (2.7 and 3)</h4>
+                    <h4>Java</h4>
+                    <h4>JavaScript</h4>
+                    <h4>JavaScript (ES6)</h4>
+                    <h4>C</h4>
+                    <h4>Node</h4>
+                  </FadeIn>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="container textAnimate">
+                  <FadeIn>
+                    <h4>MySQL</h4>
+                    <h4>MongoDB</h4>
+                    <h4>Firebase</h4>
+                    <h4>NoSQL</h4>
+                    <h4>Express</h4>
+                    <h4>Git</h4>
+                    <h4>AJAX</h4>
+                  </FadeIn>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="container textAnimate">
+                  <FadeIn>
+                    <h4>jQuery</h4>
+                    <h4>HTML5</h4>
+                    <h4>ReactJS</h4>
+                    <h4>CSS</h4>
+                    <h4>Express</h4>
+                    <h4>Web Scraping (CheerioJS)</h4>
+                    <h4>Bootstrap</h4>
+                  </FadeIn>
+                  </div>
+                </div>
+            </div>
           </FadeIn>
         </div>
     );
