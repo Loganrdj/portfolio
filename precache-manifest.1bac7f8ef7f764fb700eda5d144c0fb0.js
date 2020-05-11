a80ead1def667c7a541e9d6a47a3c28a497f59e3
Logan Moss <lrdjmoss@gmail.com>
2020-05-10 19:47:45 -0700

    Updates

---
+++ precache-manifest.1bac7f8ef7f764fb700eda5d144c0fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6daa97e03e53fefdecbf9bfa23ca84",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "96855f31b210fd9800d8",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "484f8e7fd6dc86c68012",
+    "url": "/portfolio/static/css/main.fdc0926f.chunk.css"
+  },
+  {
+    "revision": "96855f31b210fd9800d8",
+    "url": "/portfolio/static/js/2.40202f76.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.40202f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484f8e7fd6dc86c68012",
+    "url": "/portfolio/static/js/main.055e5d43.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
