16a4ca9ddae08c8382833f8f906d6590c991eba9
Logan Moss <lrdjmoss@gmail.com>
2020-05-13 20:08:42 -0700

    Updates

--- precache-manifest.9b9ad4a336e193df1e518508d8ed699e.js
+++ precache-manifest.9b9ad4a336e193df1e518508d8ed699e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "569e8c1dd978b55ea09ac45015182b8d",
+    "revision": "3c8940bf55606f2f745fd59ee9185d55",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
   },
   {
-    "revision": "69f9704755c325d368a2",
-    "url": "/portfolio/static/css/main.a553af45.chunk.css"
+    "revision": "04a4b9891095576764b0",
+    "url": "/portfolio/static/css/main.88d83463.chunk.css"
   },
   {
     "revision": "33d59ffa6074e8a03e7b",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.ab510fab.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "69f9704755c325d368a2",
-    "url": "/portfolio/static/js/main.2fa50439.chunk.js"
+    "revision": "04a4b9891095576764b0",
+    "url": "/portfolio/static/js/main.d6b608f7.chunk.js"
   },
   {
     "revision": "e5a10efc7de947a9cedd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d27cd6ff53a99964c0a776d590f479af.js"
+  "/portfolio/precache-manifest.9b9ad4a336e193df1e518508d8ed699e.js"
 );
 
 self.addEventListener('message', (event) => {
