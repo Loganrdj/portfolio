79688a3d9364a775bd6c0219fce9593a8ca41525
Logan Moss <lrdjmoss@gmail.com>
2020-05-13 20:07:59 -0700

    Added Fridgebook Project

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7c0a95e2786e32a484f611df8f7c00f5.js"
+  "/portfolio/precache-manifest.d27cd6ff53a99964c0a776d590f479af.js"
 );
 
 self.addEventListener('message', (event) => {
