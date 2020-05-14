89e8471b066d3fc58fa92b6ad0c5a257bf73177c
Logan Moss <lrdjmoss@gmail.com>
2020-05-13 21:02:32 -0700

    added working project and home pages

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -11,8 +11,8 @@ function PreviewCard(props) {
   return (
     <div className="projectCard cardAnimate">
       <img className="card-img-top" src={props.image} alt={props.alt} height="250px" width="400px"></img>
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
+      <div className="card-body projectCardBody">
+        <h5 className="card-title projectCardTitle">{props.name}</h5>
         <p className="card-text">{props.description}</p>
       </div>
       <div className="cardButtons">
