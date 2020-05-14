78644de89099b7ba7456825904e95849d6d5ae6a
Logan Moss <lrdjmoss@gmail.com>
2020-05-13 21:02:32 -0700

    added working project and home pages

---
+++ build/precache-manifest.ebbe9d09a5b71bbfcef5d58d51785861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9745ddab3dc49615a7c4134131c6c188",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9bbc1edc75b12ad1207b",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "ae2d588d6a659e38a736",
+    "url": "/portfolio/static/css/main.457ebdd7.chunk.css"
+  },
+  {
+    "revision": "9bbc1edc75b12ad1207b",
+    "url": "/portfolio/static/js/2.d87c7496.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.d87c7496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2d588d6a659e38a736",
+    "url": "/portfolio/static/js/main.78c2b218.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
