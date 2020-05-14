df72aa540b62a0c0fa4e8156a050ab1206e76cc8
Logan Moss <lrdjmoss@gmail.com>
2020-05-13 20:07:59 -0700

    Added Fridgebook Project

---
+++ build/precache-manifest.d27cd6ff53a99964c0a776d590f479af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569e8c1dd978b55ea09ac45015182b8d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "33d59ffa6074e8a03e7b",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "69f9704755c325d368a2",
+    "url": "/portfolio/static/css/main.a553af45.chunk.css"
+  },
+  {
+    "revision": "33d59ffa6074e8a03e7b",
+    "url": "/portfolio/static/js/2.ab510fab.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.ab510fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f9704755c325d368a2",
+    "url": "/portfolio/static/js/main.2fa50439.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
