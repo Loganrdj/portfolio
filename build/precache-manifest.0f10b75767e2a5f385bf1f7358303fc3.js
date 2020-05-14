3de3b677090ff49cf93960ace838ab8f8901c284
Logan Moss <lrdjmoss@gmail.com>
2020-05-14 00:04:27 -0700

    Added contact page styling

---
+++ build/precache-manifest.0f10b75767e2a5f385bf1f7358303fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eba93cef159bc806f1c48666becabb1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9e51ac535d0c306ee61d",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "d237135a84dbd501895f",
+    "url": "/portfolio/static/css/main.c5f32426.chunk.css"
+  },
+  {
+    "revision": "9e51ac535d0c306ee61d",
+    "url": "/portfolio/static/js/2.44a08ae9.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/portfolio/static/js/2.44a08ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d237135a84dbd501895f",
+    "url": "/portfolio/static/js/main.15fc8259.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
