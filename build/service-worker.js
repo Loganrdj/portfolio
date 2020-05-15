ff781b688ef1365da500d1c264c73547b0e16dbd
Logan Moss <lrdjmoss@gmail.com>
2020-05-15 01:52:20 -0700

    added resume items

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f10b75767e2a5f385bf1f7358303fc3.js"
+  "/portfolio/precache-manifest.0ca1f314b95b27197fb6c9fb08a4b17f.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,10 +6,51 @@ import { Document, Page, pdfjs } from "react-pdf";
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+ 
 
   return (
-    <div className="container">
-      
+    <div className="container resumeContainer">
+      <div className="skillsAnimate resumeItem">
+          <img src={"https://myusf.usfca.edu/sites/default/files/images/OMC/usf_f_h2_2c_rgb_360.png"}width={"30%"}></img>
+          <div className="resumeFlexBody">
+            <div>
+              <h5><strong>Major:</strong> Advertising</h5>
+              <h5><strong>Minor:</strong> Computer Science</h5>
+            </div>
+            <h5><strong>Graduation Date:</strong> May 2019</h5>
+          </div>
+      </div>
+      <div className="skillsAnimate resumeItem">
+          <img src={"https://lh3.googleusercontent.com/proxy/UdmYXhtwI5B0v1ntmC6sJ5QBg7ZAmy_dK_y7T3qCHew1mXMbg5zaFlFDycRHJc7SkZe8MKvJLHgUh_hN5_55hmRUoMWPorsuV7ZMLwIdD05PiFfZBHFq8q3jqQ"}width={"30%"}></img>
+          <div className="resumeFlexBody">
+            <h5>FullStack Development Bootcamp</h5>
+            <h5><strong>Graduation Date:</strong> May 2020</h5>
+          </div>
+      </div>
+      <div className="skillsAnimate resumeItem">
+          <img src={"https://www.wing.vc/uploads/images/companies/Bungalow_Full_BlackColor.png"}width={"30%"}></img>
+          <div className="resumeFlexBody">
+            <h5>Data Associate - Contract</h5>
+            <h5>Sep 2019 - Oct 2019</h5>
+          </div>
+      </div>
+      <div className="skillsAnimate resumeItem">
+      <img src={"https://myusf.usfca.edu/sites/default/files/images/OMC/usf_f_h2_2c_rgb_360.png"}width={"30%"}></img>
+          <div className="resumeFlexBody">
+            <div>
+              <h5>Field Support</h5>
+              <h5>Level 2 Operations</h5>
+              <h5>System Admin</h5>
+              <h5>Help Desk Specialist</h5>
+            </div>
+            <div>
+              <h5>Aug 2019 - Oct 2019</h5>
+              <h5>Jun 2019 - Aug 2019</h5>
+              <h5>Aug 2016 - Oct 2018</h5>
+              <h5>Feb 2016 - Aug 2016</h5>
+            </div>
+          </div>
+      </div>
     </div>
     // <PDFViewer
     //     document={{
