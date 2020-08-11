0c27fbd799098e8b028c95f307f304c93ce5de1c
Logan Moss <lrdjmoss@gmail.com>
2020-08-10 18:20:38 -0700

    Added popular opinion

---
+++ build/precache-manifest.4ea9c2ebc38ee2d022de2be7f27b30e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e89b3e5dc69e6cb6968184d28e1166",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07aefa170a7b7ecacff7",
+    "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "ea77eb967d0b9bf88e2d",
+    "url": "/portfolio/static/css/main.a72f4adf.chunk.css"
+  },
+  {
+    "revision": "07aefa170a7b7ecacff7",
+    "url": "/portfolio/static/js/2.683396c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.683396c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea77eb967d0b9bf88e2d",
+    "url": "/portfolio/static/js/main.ca1261f8.chunk.js"
+  },
+  {
+    "revision": "e5a10efc7de947a9cedd",
+    "url": "/portfolio/static/js/runtime-main.b67ed301.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2328f850271d0aa726b2c56ff9bf138c.js"
+  "/portfolio/precache-manifest.4ea9c2ebc38ee2d022de2be7f27b30e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
