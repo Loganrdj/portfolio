83c0b511506cfec78c6f717843677f63700d4699
Logan Moss <loganrdj@logans-mbp.gateway.sonic.net>
2020-12-19 12:49:41 -0800

    Site build

--- build/precache-manifest.87f1233495def0e29102f6ded8a9c045.js
+++ build/precache-manifest.87f1233495def0e29102f6ded8a9c045.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ce60ad60417e0f525c4a1890003db83",
+    "revision": "c515397a2209ed7315875ec013e9841c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b5c59cc19d92fcb4e384",
+    "revision": "dea17ac87d35e3d259df",
     "url": "/portfolio/static/css/2.d9ee7e2f.chunk.css"
   },
   {
-    "revision": "06d8c8d7b1db8d200d2b",
+    "revision": "c4b30a1a46021f44eedb",
     "url": "/portfolio/static/css/main.b917e295.chunk.css"
   },
   {
-    "revision": "b5c59cc19d92fcb4e384",
-    "url": "/portfolio/static/js/2.7400e546.chunk.js"
+    "revision": "dea17ac87d35e3d259df",
+    "url": "/portfolio/static/js/2.00568242.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.7400e546.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.00568242.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "06d8c8d7b1db8d200d2b",
-    "url": "/portfolio/static/js/main.1107f2bd.chunk.js"
+    "revision": "c4b30a1a46021f44eedb",
+    "url": "/portfolio/static/js/main.98945ff6.chunk.js"
   },
   {
     "revision": "e5a10efc7de947a9cedd",
