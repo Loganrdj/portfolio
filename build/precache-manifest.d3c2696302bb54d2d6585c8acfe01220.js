99d2835fd4592490403ace438a6bb1045d214e58
Logan Moss <lrdjmoss@gmail.com>
2021-06-11 13:50:50 -0400

    updated NAVBAR

---
+++ build/precache-manifest.d3c2696302bb54d2d6585c8acfe01220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b8cb2c22d583766027217690ff52c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad001b5763a6055f326",
+    "url": "/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "16a7d965951202c3cf95",
+    "url": "/static/css/main.d4417f21.chunk.css"
+  },
+  {
+    "revision": "bad001b5763a6055f326",
+    "url": "/static/js/2.275290d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.275290d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a7d965951202c3cf95",
+    "url": "/static/js/main.ea8386e3.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  }
+]);
