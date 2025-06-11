0e3a5e1487ac79c44dcde3592e8fd77de85dc9aa
L <56891756+Loganrdj@users.noreply.github.com>
2025-06-11 12:20:01 -0700

    Update projects layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,21 +10,23 @@ export default function Projects() {
 
   return (
     <>
-    <div className="row projectRow">
-      {projects.map(project => (
-        <div key={project.id} className="col-md-4 col-sm-6 col-lg-4 col-xl-3 col-12 cardCSS">
-          <ScrollAnimation delay={300} animateIn="fadeIn">
-            <img
-              src={project.image}
-              alt={project.alt}
-              className="project-thumb paused"
-              onClick={() => setSelected(project)}
-              onMouseEnter={e => e.currentTarget.classList.remove('paused')}
-              onMouseLeave={e => e.currentTarget.classList.add('paused')}
-            />
-          </ScrollAnimation>
-        </div>
-      ))}
+    <div className="projects-container">
+      <div className="projects-grid projectRow">
+        {projects.map(project => (
+          <div key={project.id} className="cardCSS">
+            <ScrollAnimation delay={300} animateIn="fadeIn">
+              <img
+                src={project.image}
+                alt={project.alt}
+                className="project-thumb paused"
+                onClick={() => setSelected(project)}
+                onMouseEnter={e => e.currentTarget.classList.remove('paused')}
+                onMouseLeave={e => e.currentTarget.classList.add('paused')}
+              />
+            </ScrollAnimation>
+          </div>
+        ))}
+      </div>
     </div>
     <ProjectModal project={selected} onClose={() => setSelected(null)} />
     </>
